7a5ad06294a236497ea85566765bd38676f5103e
Hannu Korvala <hannu.s.korvala@gmail.com>
2023-03-25 23:55:14 +0200

    Added missing annotations. Fixed problems that sonarlint complained about

--- src/main/java/com/crownedcuts/booking/WebSecurityConfig.java
+++ src/main/java/com/crownedcuts/booking/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,21 +17,22 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((requests) -> requests
+                .authorizeHttpRequests(requests -> requests
                         .anyRequest().authenticated()
                 )
-                .formLogin((form) -> form
+                .formLogin(form -> form
                         .loginPage("/login")
                         .permitAll()
                 )
-                .logout((logout) -> logout.permitAll());
+                .logout(LogoutConfigurer::permitAll);
 
         return http.build();
     }
 
     @Bean
+    @SuppressWarnings("deprecation")
     public UserDetailsService userDetailsService() {
-        UserDetails user =
+		UserDetails user =
                 User.withDefaultPasswordEncoder()
                         .username("crowned")
                         .password("cuts")
