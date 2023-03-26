c350aeed2b62f4e7f2396acfad685bf38265c9cc
Hannu Korvala <hannu.s.korvala@gmail.com>
2023-03-26 10:52:11 +0300

    Sample tests

--- src/test/java/com/crownedcuts/booking/BookingApplicationTests.java
+++ src/test/java/com/crownedcuts/booking/BookingApplicationTests.java
@@ -1,13 +1,70 @@
 package com.crownedcuts.booking;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 class BookingApplicationTests
 {
-    @Test
-    void contextLoads()
+    private final MockMvc mockMvc;
+
+    @Autowired
+    public BookingApplicationTests(MockMvc mockMvc)
     {
+        this.mockMvc = mockMvc;
+    }
+
+    @Test
+    void loginPageLoads() throws Exception
+    {
+        var request = get("/login")
+                .accept(MediaType.TEXT_HTML);
+
+        var result = mockMvc
+                .perform(request)
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String responseBody = result.getResponse().getContentAsString();
+
+        boolean validPage = responseBody.contains("<title>Spring Security Login Page</title>");
+        assertTrue(validPage);
+    }
+
+    @Test
+    void notLoggedInReturnsRedirect() throws Exception
+    {
+        var request = get("/")
+                .accept(MediaType.ALL);
+
+        mockMvc
+                .perform(request)
+                .andExpect(status().is3xxRedirection());
+    }
+
+    @Test
+    @WithMockUser("TestUser")
+    void loggedInReturnsCorrectPage() throws Exception
+    {
+        var request = get("/")
+                .accept(MediaType.TEXT_HTML);
+
+        mockMvc
+                .perform(request)
+                .andExpect(status().isOk());
     }
 }
