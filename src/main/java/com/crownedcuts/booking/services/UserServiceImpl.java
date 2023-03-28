b166cc3e9ef143e5f01720c4e17f525a25907fed
Hannu Korvala <hannu.s.korvala@gmail.com>
2023-03-28 20:43:36 +0300

    Added back transactionmanagement. Samesite to strict.

--- src/main/java/com/crownedcuts/booking/services/UserServiceImpl.java
+++ src/main/java/com/crownedcuts/booking/services/UserServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.logging.Logger;
@@ -75,6 +76,7 @@ public class UserServiceImpl implements UserService
     }
 
     @Override
+    @Transactional
     public boolean addUser(UserDetails user)
     {
         try
