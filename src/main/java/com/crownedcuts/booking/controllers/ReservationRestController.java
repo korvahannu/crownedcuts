3d044c56d0e02b973bff4143f99f04719a56f4f0
Hannu Korvala <hannu.s.korvala@gmail.com>
2023-04-07 10:20:08 +0300

    Rest endpoints for getting barbers and barber available times

---
+++ src/main/java/com/crownedcuts/booking/controllers/ReservationRestController.java
@@ -0,0 +1,36 @@
+package com.crownedcuts.booking.controllers;
+
+import com.crownedcuts.booking.records.AvailableTime;
+import com.crownedcuts.booking.records.BarberHairdresser;
+import com.crownedcuts.booking.services.BarberHairdresserService;
+import com.crownedcuts.booking.services.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ReservationRestController
+{
+    private final ReservationService reservationService;
+    private final BarberHairdresserService barberHairdresserService;
+
+    @Autowired
+    public ReservationRestController(ReservationService reservationService, BarberHairdresserService barberHairdresserService) {
+        this.reservationService = reservationService;
+        this.barberHairdresserService = barberHairdresserService;
+    }
+
+    @GetMapping("/rest/getBarbers")
+    public List<BarberHairdresser> onGetBarbers() {
+        return barberHairdresserService.getAllBarbers();
+    }
+
+    @GetMapping("/rest/getAvailableTimes")
+    public List<AvailableTime> getAvailableTimes(@RequestParam int year, @RequestParam int month, @RequestParam int day)
+    {
+        return reservationService.getAllFreeTimesOnDay(year, month, day);
+    }
+}
