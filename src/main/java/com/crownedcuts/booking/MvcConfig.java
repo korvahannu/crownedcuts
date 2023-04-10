965599602ebb93dcb6fd31c5d3d063d2845e4cf5
Hannu Korvala <hannu.s.korvala@gmail.com>
2023-04-10 11:05:43 +0300

    .idea settings for character encoding. Added i18n for /ajanvarausonnistui. Added initial i18n for /ajanvaraus

--- src/main/java/com/crownedcuts/booking/MvcConfig.java
+++ src/main/java/com/crownedcuts/booking/MvcConfig.java
@@ -4,8 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer
@@ -22,5 +28,27 @@ public class MvcConfig implements WebMvcConfigurer
     public PasswordEncoder encoder()
     {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public LocaleResolver localeResolver()
+    {
+        var slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(new Locale("fi", "FI"));
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor()
+    {
+        var lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry)
+    {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 }
