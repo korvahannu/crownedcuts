769c4d08a4e3ccca9408ec8e8ed6a574b52e468e
Milla Pyrrö <milla.pyrro@student.oulu.fi>
2023-04-29 13:59:13 +0300

    made page for checking all earlier and upcoming reservations

--- src/main/java/com/crownedcuts/booking/controllers/AllReservationsController.java
+++ src/main/java/com/crownedcuts/booking/controllers/AllReservationsController.java
@@ -10,35 +10,37 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 import com.crownedcuts.booking.services.UserService;
 import com.fasterxml.jackson.datatype.jsr310.ser.ZonedDateTimeSerializer;
-
 import ch.qos.logback.core.model.Model;
-
 import com.crownedcuts.booking.records.Reservation;
+import com.crownedcuts.booking.records.TimeDetails;
 import com.crownedcuts.booking.records.UserDetails;
+import org.springframework.stereotype.Controller;
 
 
-public class AllReservationsController {
+@Controller
+public class AllreservationsController {
 
     private static final String VIEW_NAME = "allreservations";
 
     private final UserService userService;
 
     @Autowired
-    public AllReservationsController(UserService userService) {
+    public AllreservationsController(UserService userService) {
         this.userService = userService;
     }
     
     @GetMapping(value = {"/allreservations", "/kaikkiajanvaraukset"})
     public ModelAndView onGet()
     {
+        System.out.println("/allreservations " + VIEW_NAME);
         var mvc = new ModelAndView(VIEW_NAME);
 
         var authentication = SecurityContextHolder.getContext().getAuthentication();
         var username = ((UserDetails) authentication.getPrincipal()).username();
 
         List<Reservation> allreservations = userService.getReservations(username);
-        List<Reservation> earlierreservations = new ArrayList<>();
-        List<Reservation> upcomingreservations = new ArrayList<>();
+        List<TimeDetails> earlierreservations = new ArrayList<>();
+        List<TimeDetails> upcomingreservations = new ArrayList<>();
 
         for(int i = 0; i < allreservations.size(); i++) {
             LocalDateTime now = LocalDateTime.now();
@@ -46,9 +48,9 @@ public class AllReservationsController {
             var localDateTime = LocalDateTime.of(timeDetails.year(), timeDetails.month(), timeDetails.day(), timeDetails.hour(), 0);
 
             if(now.isBefore(localDateTime)) {
-                earlierreservations.add(allreservations.get(i));
+                earlierreservations.add(allreservations.get(i).reservationInformation());
             } else {
-                upcomingreservations.add(allreservations.get(i));
+                upcomingreservations.add(allreservations.get(i).reservationInformation());
             }
         }
 
--- src/main/java/com/crownedcuts/booking/controllers/IndexController.java
+++ src/main/java/com/crownedcuts/booking/controllers/IndexController.java
@@ -1,19 +1,38 @@
 package com.crownedcuts.booking.controllers;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.crownedcuts.booking.records.Reservation;
+import com.crownedcuts.booking.records.TimeDetails;
+import com.crownedcuts.booking.records.UserDetails;
+import com.crownedcuts.booking.services.UserService;
+
 @Controller
 public class IndexController
 {
+
+    private final UserService userService;
+
+    @Autowired
+    public IndexController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping(value = {"/", "/index"})
     public ModelAndView onGet()
     {
         ModelAndView mav = new ModelAndView("index");
 
         var auth = SecurityContextHolder.getContext().getAuthentication();
+        var username = ((UserDetails) auth.getPrincipal()).username();
 
         try
         {
@@ -23,6 +42,21 @@ public class IndexController
         {
             mav.addObject("userDetails", "anonymous user");
         }
+
+        List<Reservation> allreservations = userService.getReservations(username);
+        List<TimeDetails> upcomingreservations = new ArrayList<>();
+
+        for(int i = 0; i < allreservations.size(); i++) {
+            LocalDateTime now = LocalDateTime.now();
+            var timeDetails = allreservations.get(i).reservationInformation();
+            var localDateTime = LocalDateTime.of(timeDetails.year(), timeDetails.month(), timeDetails.day(), timeDetails.hour(), 0);
+
+            if(now.isAfter(localDateTime)) {
+                upcomingreservations.add(allreservations.get(i).reservationInformation());
+            }
+        }
+
+        mav.addObject("upcomingreservationslist", upcomingreservations);
 
         return mav;
     }
