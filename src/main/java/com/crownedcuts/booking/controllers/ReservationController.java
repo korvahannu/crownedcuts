41b41c5e72efbed63b28f1f122fb762a21143694
Hannu Korvala <hannu.s.korvala@gmail.com>
2023-05-02 18:05:43 +0300

    Fix reserving

--- src/main/java/com/crownedcuts/booking/controllers/ReservationController.java
+++ src/main/java/com/crownedcuts/booking/controllers/ReservationController.java
@@ -53,8 +53,7 @@ public class ReservationController
 
         var barber = barberHairdresserService.getBarber(payload.barberId()).orElseThrow();
         var timeDetails = new TimeDetails(payload.year(), payload.month(), payload.day(), payload.hour());
-        var username = SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
-        var user = UserDetails.of(username, null, null, null);
+        var user = (UserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
         if (reservationService.reserveTime(barber, user, timeDetails, payload.hairLength(), payload.services()))
         {
