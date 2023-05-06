c922e2c98a77f25599a5a834352e836c266b5627
Milla Pyrrö <milla.pyrro@student.oulu.fi>
2023-05-06 10:12:50 +0300

    small changes to allreservationscontroller and indexcontroller

--- src/main/java/com/crownedcuts/booking/controllers/AllReservationsController.java
+++ src/main/java/com/crownedcuts/booking/controllers/AllReservationsController.java
@@ -1,33 +1,34 @@
 package com.crownedcuts.booking.controllers;
-
-import com.crownedcuts.booking.records.Reservation;
-import com.crownedcuts.booking.records.UserDetails;
-import com.crownedcuts.booking.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
-
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+import com.crownedcuts.booking.services.UserService;
+import com.fasterxml.jackson.datatype.jsr310.ser.ZonedDateTimeSerializer;
+import ch.qos.logback.core.model.Model;
+import com.crownedcuts.booking.records.Reservation;
+import com.crownedcuts.booking.records.TimeDetails;
+import com.crownedcuts.booking.records.UserDetails;
+import org.springframework.stereotype.Controller;
+
 
 @Controller
-public class AllReservationsController
-{
+public class AllReservationsController {
 
     private static final String VIEW_NAME = "allreservations";
 
     private final UserService userService;
 
     @Autowired
-    public AllReservationsController(UserService userService)
-    {
+    public AllReservationsController(UserService userService) {
         this.userService = userService;
     }
-
+    
     @GetMapping(value = {"/allreservations", "/kaikkiajanvaraukset"})
     public ModelAndView onGet()
     {
@@ -40,24 +41,16 @@ public class AllReservationsController
         List<String> earlierreservations = new ArrayList<>();
         List<String> upcomingreservations = new ArrayList<>();
 
-        for (int i = 0; i < allreservations.size(); i++)
-        {
+        for(int i = 0; i < allreservations.size(); i++) {
             LocalDateTime now = LocalDateTime.now();
             var timeDetails = allreservations.get(i).reservationInformation();
-            var localDateTime = LocalDateTime.of(timeDetails.year(),
-                    timeDetails.month(),
-                    timeDetails.day(),
-                    timeDetails.hour(),
-                    0);
-            String date = timeDetails.day() + "." + timeDetails.month() + "." + timeDetails.year() + " klo " + timeDetails.hour();
+            var localDateTime = LocalDateTime.of(timeDetails.year(), timeDetails.month(), timeDetails.day(), timeDetails.hour(), 0);
+            String date = timeDetails.day() + "." + timeDetails.month() + "." + timeDetails.year() + " " + timeDetails.hour() + ":00";
 
 
-            if (now.isBefore(localDateTime))
-            {
+            if(now.isBefore(localDateTime)) {
                 upcomingreservations.add(date);
-            }
-            else
-            {
+            } else {
                 earlierreservations.add(date);
             }
         }
