bbc2185bb5cf5e12bf8eff8ec6e8b2b10e9273b8
Hannu Korvala <hannu.s.korvala@gmail.com>
2023-05-10 18:38:59 +0300

    Enabled TLS / HTTPS with self-signed certificate

--- src/main/java/com/crownedcuts/booking/BookingApplication.java
+++ src/main/java/com/crownedcuts/booking/BookingApplication.java
@@ -1,13 +1,54 @@
 package com.crownedcuts.booking;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookingApplication
 {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${server.http.port}")
+    private String serverHttpPort;
+
     public static void main(String[] args)
     {
         SpringApplication.run(BookingApplication.class, args);
+    }
+
+    @Bean
+    public ServletWebServerFactory servletContainer()
+    {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                var securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                var collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(getHttpConnector());
+        return tomcat;
+    }
+
+    private Connector getHttpConnector() {
+        var connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(Integer.parseInt(serverHttpPort));
+        connector.setSecure(false);
+        connector.setRedirectPort(Integer.parseInt(serverPort));
+        return connector;
     }
 }
--- src/main/java/com/crownedcuts/booking/MvcConfig.java
+++ src/main/java/com/crownedcuts/booking/MvcConfig.java
@@ -4,13 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-
 import java.util.Locale;
 
 @Configuration
